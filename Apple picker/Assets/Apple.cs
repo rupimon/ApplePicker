67bb5bedba8aa146fbbc33ac51abc57e4c57de6e
Rupanjal Dasgupta <rupimon@gmail.com>
2015-09-18 22:52:44 -0400

    unfinished apple picker

--- Apple picker/Assets/Apple.cs
+++ Apple picker/Assets/Apple.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class Apple : MonoBehaviour {
 
 	// Use this for initialization
+	public static float bottomY = -20f;
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (transform.position.y < bottomY) {
+			Destroy (this.gameObject);
+		}
 	}
 }
