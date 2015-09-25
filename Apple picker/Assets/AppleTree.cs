b3d0f53b12aa7fff26547e6c3fb86227e6fd86af
Rupanjal Dasgupta <rupimon@gmail.com>
2015-09-24 20:29:25 -0400

    Final Apple Picker

--- Apple picker/Assets/AppleTree.cs
+++ Apple picker/Assets/AppleTree.cs
@@ -16,7 +16,7 @@ public class AppleTree : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 pos = transform.position;
-		pos.x += speed + Time.deltaTime;
+		pos.x += speed * Time.deltaTime;
 		transform.position = pos;
 		if (pos.x < -leftAndRightEdge) {
 			speed = Mathf.Abs (speed);
